bcab68a030a0f5397748daca8319dfd27067bfc7
Sean Kelleher <ezanmoto@gmail.com>
2014-03-09 23:22:37 +0000

    Use `bin/plot.py` default values for `-s` and `-n`

--- code/matrix/build.sh
+++ code/matrix/build.sh
@@ -20,10 +20,10 @@ fi
 
 for CC in $* ; do
 	for OPT in 0 1 2 3 ; do
-		run bin/plot.py -f smul,mmul,mmul_s,row_reduce_s -s 100 -n 10 -c $CC -o $OPT
+		run bin/plot.py -f smul,mmul,mmul_s,row_reduce_s -c $CC -o $OPT
 	done
 
 	for IMPL in smul mmul mmul_s row_reduce_s ; do
-		run bin/plot.py -f $IMPL -s 100 -n 10 -c $CC -o 0,1,2
+		run bin/plot.py -f $IMPL -c $CC -o 0,1,2
 	done
 done
